ac1f18127e6c85ea38be20827ec491d85305849b
Rayan20107 <75358640+Rayan20107@users.noreply.github.com>
2021-08-26 14:33:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+song=""
+
+function draw()
+{
+    image(video, 0, 0, 500, 500);
+}
+
+function preload()
+{
+    song=loadSound("music.mp3")
+}
+
+function setup()
+{
+    canvas=createCanvas(500, 500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function play()
+{
+    song.play();
+}
